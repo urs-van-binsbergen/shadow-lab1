506e73361dfb755b174def53a59fe35618ba5e3c
Urs van Binsbergen <urs.vanbinsbergen@zhdk.ch>
2023-10-07 18:45:31 +0200

    add reagent / react 18

--- src/main/lab1/frontend/app.cljs
+++ src/main/lab1/frontend/app.cljs
@@ -1,4 +1,18 @@
-(ns lab1.frontend.app)
+(ns lab1.frontend.app
+  (:require [reagent.core :as r]
+            ["react-dom/client" :refer [createRoot]]))
+
+(defonce root (createRoot (js/document.getElementById "app")))
+
+(defn- main []
+  [:ul
+   [:li "Hello"]
+   [:li {:style {:color "green"}} "World!"]])
+
+(defn ^:dev/after-load start []
+  (println "start")
+  (.render root (r/as-element [main])))
 
 (defn init []
-  (println "Hello World"))
+  (println "init")
+  (start))

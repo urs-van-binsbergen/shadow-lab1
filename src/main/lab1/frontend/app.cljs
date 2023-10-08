fe2e966d59084daef4392ee676a56afe2f377dc1
Urs van Binsbergen <urs.vanbinsbergen@zhdk.ch>
2023-10-08 15:01:38 +0200

    add reitit

--- src/main/lab1/frontend/app.cljs
+++ src/main/lab1/frontend/app.cljs
@@ -1,18 +1,58 @@
 (ns lab1.frontend.app
   (:require [reagent.core :as r]
-            ["react-dom/client" :refer [createRoot]]))
+            ["react-dom/client" :refer [createRoot]]
+            [reitit.frontend :as reitit-frontend]
+            [reitit.frontend.easy :as reitit-easy]
+            [reitit.coercion.spec :as reitit-coercion-spec]
+            [spec-tools.data-spec :as ds]))
 
 (defonce root (createRoot (js/document.getElementById "app")))
+(defonce routes-state (r/atom nil))
 
-(defn- main []
+(defn nav []
   [:ul
-   [:li "Hello"]
-   [:li {:style {:color "green"}} "World!"]])
+   [:li [:a {:href (reitit-easy/href ::home)} "Home"]]
+   [:li [:a {:href (reitit-easy/href ::login)} "Login"]]
+   [:li [:a {:href "/unknown"} "Unknown Route"]]])
 
-(defn ^:dev/after-load start []
-  (println "start")
-  (.render root (r/as-element [main])))
+(defn home-page []
+  (println "render home-page")
+  [:div
+   [:h3 "home-page"]])
+
+(defn login-page []
+  (println "render login-page")
+  [:div
+   [:h3 "login-page"]])
+
+(defn app-view []
+  [:div
+   [nav]
+   [:h2 "render app-view"]
+   (if-let [current-view (-> @routes-state :data :view)]
+     [current-view]
+     [:div "unknown route"])])
+
+(defn ^:dev/after-load render []
+  (println "render root")
+  (.render root (r/as-element [app-view])))
+
+(def routes
+  [["/" {:name ::home
+         :view #'home-page}]
+   ["/login" {:name ::login
+              :view #'login-page}]])
+
+(defn router-start! []
+  (reitit-easy/start!
+   ; router
+   (reitit-frontend/router routes {:data {:coercion reitit-coercion-spec/coercion}})
+   ; on-navigate
+   (fn [matched-route] (reset! routes-state matched-route)) ; bei reitit-example: "match"
+   ; opts
+   {:use-fragment false}))
 
 (defn init []
   (println "init")
-  (start))
+  (router-start!)
+  (render))

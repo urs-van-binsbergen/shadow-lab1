2b445aece511d15746a7783485bed8798b588cf4
uvb <urs.vanbinsbergen@zhdk.ch>
2023-11-16 10:35:38 +0100

    16.11.2023

--- src/main/lab1/frontend/pages/zoos.cljs
+++ src/main/lab1/frontend/pages/zoos.cljs
@@ -1,29 +1,39 @@
 (ns lab1.frontend.pages.zoos
-  (:require
-   [reagent.core :as r]
-   [cljs.core.async :refer [go]]
-   [cljs.core.async.interop :refer-macros [<p!]]))
+  (:require-macros [cljs.core.async.macros :refer [go]])
+  (:require [reagent.core :as r]
+            [cljs-http.client :as http]
+            [cljs.core.async :refer [<!]]
+            #_[cljs.core.async.interop :refer-macros [<p!]] ; promise-to-async
+            ))
 
-#_(go
-    (-> (fn [resolve reject] (js/setTimeout #(resolve 42) 2000))
-        js/Promise.
-        <p!
-        println))
 
-(defn zoos-1 []
-  (let [data (r/atom nil)]
+(defn zoos-cljs-http []
+  (let [zoos (r/atom [])]
+    (go (let [response (<! (http/get "http://localhost:3001/zoos"))]
+          (reset! zoos (:body response))))
+    (fn []
+      (js/console.log "render" @zoos)
+      [:div
+       [:h4 "Zoos with cljs-http"]
+       [:button {:on-click #(swap! zoos conj {:id 0 :name "Foo"})} "Add"]
+       (for [zoo @zoos]
+         [:div {:key (zoo :id)} (zoo :id) " " (zoo :name)])])))
+
+(defn zoos-js-fetch []
+  (let [zoos-data (r/atom nil)]
     (-> (js/fetch "http://localhost:3001/zoos")
         (.then #(.json %))
-        (.then #(reset! data (js->clj %))))
+        (.then #(reset! zoos-data (js->clj %))))
     (fn []
       [:div
        [:h4 "Zoos with js/fetch and Promise"]
-       [:button {:on-click #(swap! data conj {"id" 0 "name" "Foo"})} "Add"]
-       (when-let [zoos @data]
+       [:button {:on-click #(swap! zoos-data conj {"id" 0 "name" "Foo"})} "Add"]
+       (when-let [zoos @zoos-data]
          (for [zoo zoos]
            [:div {:key (zoo "id")} (zoo "id") " " (zoo "name")]))])))
 
 (defn index-page []
   [:<>
    [:h3 "Zoos"]
-   [zoos-1]])
+   [zoos-js-fetch]
+   [zoos-cljs-http]])

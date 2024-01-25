5701834a11281ac65373b249b5c7a709f8ff137a
uvb <urs.vanbinsbergen@zhdk.ch>
2024-01-25 16:42:37 +0100

    add re-frame to deps

--- src/main/lab1/frontend/pages/home.cljs
+++ src/main/lab1/frontend/pages/home.cljs
@@ -1,9 +1,18 @@
 (ns lab1.frontend.pages.home
   (:require
-   [lab1.frontend.components.counter-component :refer [counter-component]]))
+   [lab1.frontend.components.counter-component :refer [counter-component]]
+   [reagent.core :as r]))
+
+(defn timer-component []
+  (let [seconds-elapsed (r/atom 0)]
+    (fn []
+      (js/setTimeout #(swap! seconds-elapsed inc) 1000)
+      [:div
+       "Seconds Elapsed: " @seconds-elapsed])))
 
 (defn page []
   [:div
    [:h3 "home-page"]
-   [:div "hello" [counter-component]]
-   [:div "hello" [counter-component]]])
+   [timer-component]
+   [:div "hello " [counter-component]]
+   [:div "hello " [counter-component]]])
